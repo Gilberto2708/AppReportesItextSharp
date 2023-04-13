ecc06c41fa40f9b6b10e552033883750d15e59f9
Gilberto Cabrera <gilbertocabreravargas@gmail.com>
2023-04-12 21:59:41 -0700

    Subiendo reporteador

---
+++ ReporteiTextSharp.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace waiTextSharp
+{
+  partial class ReporteiTextSharp
+  {
+    /// <summary>
+    /// Variable del diseñador necesaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpiar los recursos que se estén usando.
+    /// </summary>
+    /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código generado por el Diseñador de Windows Forms
+
+    /// <summary>
+    /// Método necesario para admitir el Diseñador. No se puede modificar
+    /// el contenido de este método con el editor de código.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReporteiTextSharp));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tssMensaje = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbImprimir = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tscboInformacion = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtExcepcion = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.tssMensaje});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 329);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(600, 37);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Image = global::waiTextSharp.Properties.Resources.Print_16x;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(162, 32);
+            this.toolStripStatusLabel1.Text = "Vidal TI Consultor, 2021";
+            // 
+            // tssMensaje
+            // 
+            this.tssMensaje.Name = "tssMensaje";
+            this.tssMensaje.Size = new System.Drawing.Size(140, 32);
+            this.tssMensaje.Text = " - [ Estado del proceso... ]";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbImprimir,
+            this.tsbSalir,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.tscboInformacion,
+            this.toolStripSeparator2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(600, 39);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Menu_ItemClicked);
+            // 
+            // tsbImprimir
+            // 
+            this.tsbImprimir.Image = global::waiTextSharp.Properties.Resources.Print_16x;
+            this.tsbImprimir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbImprimir.Name = "tsbImprimir";
+            this.tsbImprimir.Size = new System.Drawing.Size(89, 36);
+            this.tsbImprimir.Text = "&Imprimir";
+            // 
+            // tsbSalir
+            // 
+            this.tsbSalir.Image = global::waiTextSharp.Properties.Resources.Exit_16x;
+            this.tsbSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalir.Name = "tsbSalir";
+            this.tsbSalir.Size = new System.Drawing.Size(65, 36);
+            this.tsbSalir.Text = "&Salir";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(107, 36);
+            this.toolStripLabel1.Text = "Obtener reporte de";
+            // 
+            // tscboInformacion
+            // 
+            this.tscboInformacion.AutoCompleteCustomSource.AddRange(new string[] {
+            "Empleado",
+            "Persona",
+            "Prodcuto"});
+            this.tscboInformacion.Items.AddRange(new object[] {
+            "Empleado",
+            "Persona",
+            "Producto"});
+            this.tscboInformacion.Name = "tscboInformacion";
+            this.tscboInformacion.Size = new System.Drawing.Size(92, 39);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 39);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtExcepcion);
+            this.groupBox1.Location = new System.Drawing.Point(10, 203);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(581, 119);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Excepción";
+            // 
+            // txtExcepcion
+            // 
+            this.txtExcepcion.Location = new System.Drawing.Point(4, 17);
+            this.txtExcepcion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtExcepcion.Multiline = true;
+            this.txtExcepcion.Name = "txtExcepcion";
+            this.txtExcepcion.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtExcepcion.Size = new System.Drawing.Size(573, 98);
+            this.txtExcepcion.TabIndex = 0;
+            // 
+            // ReporteiTextSharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "ReporteiTextSharp";
+            this.Text = "Generar reportes PDF con iTextSharp";
+            this.Load += new System.EventHandler(this.ReporteiTextSharp_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    private System.Windows.Forms.ToolStrip toolStrip1;
+    private System.Windows.Forms.ToolStripButton tsbImprimir;
+    private System.Windows.Forms.ToolStripButton tsbSalir;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    private System.Windows.Forms.ToolStripComboBox tscboInformacion;
+    private System.Windows.Forms.ToolStripStatusLabel tssMensaje;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.TextBox txtExcepcion;
+  }
+}
+
---
+++ ReporteiTextSharp.cs
@@ -0,0 +1,282 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using waiTextSharp.utilidades;
+
+namespace waiTextSharp
+{
+
+  public partial class ReporteiTextSharp : Form
+  {
+
+    #region "variables"
+
+    /// <summary>
+    /// Obtener la ruta y nombre logotipo 
+    /// </summary>
+    public string ArchivoLogotipo = Comun.AppRuta() + Comun.AppLogotipo;
+
+    /// <summary>
+    /// Obtener la ruta donde se crea y guarda el reporte (archivo) PDF
+    /// </summary>
+    public string RutaReporte = Comun.AppRuta() + Comun.AppRutaReporte;
+
+    /// <summary>
+    /// Obtener la cadena de conexión desde App.config, para conexíón con SQL Server 
+    /// </summary>
+    public string CadenaConexion = System.Configuration.ConfigurationManager.ConnectionStrings["csMSQLServer"].ConnectionString;
+
+    #endregion 
+
+    #region "constructor"
+
+    public ReporteiTextSharp()
+    {
+      InitializeComponent();
+    }
+
+    #endregion 
+
+    #region "barra de herramientas"
+
+    private void Menu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+    {
+      try
+      {
+        // verificar
+        switch (e.ClickedItem.Name)
+        {
+          case "tsbImprimir":
+            // verificar
+            if (tscboInformacion.SelectedItem != null)
+            {
+              ImprimirInformacion(tscboInformacion.SelectedItem.ToString().ToUpper());
+            }
+            else
+            {
+              MessageBox.Show("Seleccione una opción de la lista Información.", Comun.AppNombre(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            break;
+          case "tsbSalir":
+            // salir
+            Application.Exit();
+            break;
+          default:
+            // salir
+            Application.Exit();
+            break;
+        }
+
+      } catch (Exception Ex)
+      {
+        // excepción
+        txtExcepcion.Text = Ex.ToString();
+        // mensaje 
+        MensajeMostrar(" - [ Ha ocurrido una Excepción... ]");
+      }
+    }
+
+    #endregion 
+
+    #region "imprimir"
+
+    /// <summary>
+    /// Genera reporte PDF con la información de la tabla solicitada
+    /// </summary>
+    /// <param name="TablaImnprimir">Nombre de la tabla</param>
+    private void ImprimirInformacion(string TablaImnprimir)
+    {
+      SqlConnection conTmp = new SqlConnection();
+      SqlCommand cmdTmp = new SqlCommand();
+      SqlDataAdapter daTmp;
+      DataSet dstTmp = new DataSet();
+      string ArchivoNombre;
+      string Encabezado;
+      string Subencabezado = "";
+      string PiePagina = "";
+      ArrayList arlColumnas = new ArrayList();
+      Rectangle PapelTamanio = iTextSharp.text.PageSize.LETTER;
+      try
+      {
+
+        // cambiar puntero del ratón
+        Cursor.Current = Cursors.WaitCursor;
+
+        // mensaje inicializar
+        MensajeLimpiar();
+
+        // mensaje
+        MensajeMostrar(" - [ Reporte en proceso... ]");
+
+        // ruta y nombre del archivo con extensión
+        ArchivoNombre = RutaReporte + "rpt" + TablaImnprimir + DateTime.Now.ToFileTime().ToString() + ".pdf";
+
+        // Encabezado
+        Encabezado = Comun.AppNombre();
+
+        // instanciar conexion
+        using (conTmp = new SqlConnection(CadenaConexion))
+        {
+          // abrir
+          conTmp.Open();
+
+          // verificar
+          switch (TablaImnprimir)
+          {
+            case "EMPLEADO":
+              // preparar comando
+              cmdTmp = new SqlCommand("SELECT HumanResources.Employee.BusinessEntityID, ISNULL(Person.Person.FirstName, '') + ' ' + ISNULL(Person.Person.MiddleName, '') + ' ' + ISNULL(Person.Person.LastName, '') AS Name, JobTitle, BirthDate, CASE MaritalStatus WHEN 'S' THEN 'Soltero' WHEN 'M' THEN 'Casado' ELSE '' END AS MaritalStatus, CASE Gender WHEN 'F' THEN 'Mujer' WHEN 'M' THEN 'Hombre' ELSE '' END AS Gender, HireDate FROM HumanResources.Employee LEFT JOIN Person.Person ON HumanResources.Employee.BusinessEntityID = Person.Person.BusinessEntityID ORDER BY HumanResources.Employee.BusinessEntityId", conTmp);
+
+              // tamaño de la hoja
+              PapelTamanio = iTextSharp.text.PageSize.LETTER;
+
+              // encabezado
+              Subencabezado = "Reporte de Empleados";
+
+              // columnas
+              arlColumnas.Add(new ReporteColumna("Identificador", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Nombre", 25, true, Element.ALIGN_CENTER, Element.ALIGN_LEFT, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Puesto", 25, true, Element.ALIGN_CENTER, Element.ALIGN_LEFT, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Fecha nacimiento", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "d", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Estado civil", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Género", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Fecha contratación", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "d", FontFactory.TIMES_ROMAN, 8));
+
+              // pie de página
+              PiePagina = "Puedes poner un mensaje en el pie de página";
+              break;
+            case "PERSONA":
+              // preparar comando
+              cmdTmp = new SqlCommand("SELECT BusinessEntityID, PersonType, ISNULL(Person.Person.FirstName, '') + ' ' + ISNULL(Person.Person.MiddleName, '') + ' ' + ISNULL(Person.Person.LastName, '') AS Name, ModifiedDate FROM Person.Person WHERE BusinessEntityID <= @BusinessEntityID", conTmp);
+
+              // adicionar parametro
+              cmdTmp.Parameters.Add("@BusinessEntityID", SqlDbType.Int).Value = 300;
+
+              // tamaño de la hoja
+              PapelTamanio = iTextSharp.text.PageSize.LETTER;
+
+              // encabezado
+              Subencabezado = "Reporte de Personas";
+
+              // columnas
+              arlColumnas.Add(new ReporteColumna("Identificador", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Tipo de persona", 12, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Nombre", 66, true, Element.ALIGN_CENTER, Element.ALIGN_LEFT, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Fecha modificación", 12, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "d", FontFactory.TIMES_ROMAN, 8));
+
+              // pie de página
+              PiePagina = "";
+              break;
+            case "PRODUCTO":
+              // preparar comando
+              //cmdTmp = new SqlCommand("SELECT ProductID, Name, ProductNumber, Color, SafetyStockLevel, ReorderPoint, StandardCost FROM Production.Product WHERE ProductID <= @ProductID ORDER BY ProductID", conTmp);
+              cmdTmp = new SqlCommand("SELECT ProductID, Name, ProductNumber, Color, SafetyStockLevel, ReorderPoint, StandardCost, ListPrice FROM Production.Product WHERE ProductID <= @ProductID ORDER BY ProductID", conTmp);
+
+              // adicionar parametro
+              cmdTmp.Parameters.Add("@ProductID", SqlDbType.Int).Value = 500;
+
+              // tamaño de la hoja
+              PapelTamanio = iTextSharp.text.PageSize.LETTER.Rotate();
+
+              // encabezado
+              Subencabezado = "Reporte de Productos";
+
+              // columnas
+              arlColumnas.Add(new ReporteColumna("Identificador", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Nombre", 28, true, Element.ALIGN_CENTER, Element.ALIGN_LEFT, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Número", 12, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Color", 10, true, Element.ALIGN_CENTER, Element.ALIGN_CENTER, "", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Existencia mínima", 13, true, Element.ALIGN_CENTER, Element.ALIGN_RIGHT, "N0", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Punto de pedido", 13, true, Element.ALIGN_CENTER, Element.ALIGN_RIGHT, "N0", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Costo", 13, true, Element.ALIGN_CENTER, Element.ALIGN_RIGHT, "N2", FontFactory.TIMES_ROMAN, 8));
+              arlColumnas.Add(new ReporteColumna("Precio de lista", 13, true, Element.ALIGN_CENTER, Element.ALIGN_RIGHT, "C2", FontFactory.TIMES_ROMAN, 8));
+
+              // pie de página
+              PiePagina = "";
+              break;
+          }
+
+          // obtener informacion
+          daTmp = new SqlDataAdapter(cmdTmp);
+          daTmp.Fill(dstTmp);
+
+          // asignar el nombre a la tabla
+          dstTmp.Tables[0].TableName = TablaImnprimir;
+
+          // instanciar reporte
+          Reporte udtReporte = new Reporte(ArchivoLogotipo);
+
+          // crear reporte
+          udtReporte.Generar(ArchivoNombre, PapelTamanio, Encabezado, Subencabezado, PiePagina, arlColumnas, dstTmp.Tables[0]);
+
+          // entregar el reporte con la aplicación asociada
+          System.Diagnostics.Process.Start(ArchivoNombre);
+        }
+
+        // mensaje
+        MensajeMostrar(" - [ Reporte terminado... ]");
+
+        // cambiar puntero ratón
+        Cursor.Current = Cursors.Default;
+      }
+      catch (Exception Ex)
+      {
+        // heredar
+        throw Ex;
+      }
+      finally {
+        // cerrar y destruir
+        cmdTmp.Dispose();
+        conTmp.Close();
+        conTmp.Dispose();
+      }
+    }
+
+    /// <summary>
+    /// Mostrar mensaje en la barra de estado
+    /// </summary>
+    /// <param name="mensaje">Mensaje a mortrar</param>
+    private void MensajeMostrar(string mensaje)
+    {
+      try
+      {
+        // mostrar
+        tssMensaje.Text = mensaje;
+        tssMensaje.ForeColor = mensaje.StartsWith(" - [ Ha ocurrido una Excepción... ]") ? System.Drawing.Color.Red : System.Drawing.Color.DodgerBlue;
+      }
+      catch (Exception Ex)
+      {
+        // heredar
+        throw Ex;
+      }
+    }
+
+    /// <summary>
+    /// Inicializar el control del mensaje
+    /// </summary>
+    private void MensajeLimpiar()
+    {
+      try
+      {
+        // mostrar
+        tssMensaje.Text = "";
+        tssMensaje.ForeColor = DefaultForeColor;
+      }
+      catch (Exception Ex)
+      {
+        // heredar
+        throw Ex;
+      }
+    }
+
+        #endregion
+
+        private void ReporteiTextSharp_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
